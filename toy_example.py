95787f352dbe72cb49b1cade6dff0ad0734a4786
Daji Landis <daji@ucla.edu>
2023-03-10 13:22:01 +0100

    got the verification out of main, started on randomness fix

--- toy_example.py
+++ toy_example.py
@@ -1,6 +1,9 @@
 import random
 from datetime import datetime
 import math
+from enum import Enum
+import hashlib
+
 
 # defining transfer function between players/player and contract
 def transfer(sender, receiver, amount):
@@ -18,6 +21,14 @@ def coin_flip(p = 0.5):
         return 1
     else:
         return 0
+
+def hash(bit):
+    if type(bit) is not bin:
+        bit = bin(bit)
+    m = hashlib.sha256()
+    m.update(b"Nobody inspects the spammish repetition")
+    m.hexdigest()
+    return m
 
 # defining player's information for computation
 class InfoPacket:
@@ -50,27 +61,28 @@ class Verifier:
     def __init__(self, wallet, circuit_info):
         self.wallet = wallet
         self.circuit_info = circuit_info
-        self.proof_info = InfoPacket()
-
-    # def verify(self):
-    #     my_input = self.circuit_info.data
-    #     proof_output = self.proof_info.evaluate()
-    #     print("my input " + str(my_input)+"\nproof output " + str(proof_output))
-    #     if my_input == proof_output:
-    #         return True
-    #     else:
-    #         print("boooo you lied")
-    #         return False
+        self.proof_info = 0
+        self.bit = coin_flip(p=0.5)
+        self.hash = hash(self.bit)
 
 class Prover:
     def __init__(self, wallet, proof):
         self.wallet = wallet
         self.circuit_info = InfoPacket()
         self.proof_info = InfoPacket(0, proof)
+        self.bit = coin_flip(p=0.5)
+        self.hash = hash(self.bit)
 
     def do_computation(self):
         # do the computation and save to proof info packet
         self.proof_info.data = self.circuit_info.evaluate()
+
+class contract_states(Enum):
+    start = 1
+    committed = 2
+    verify = 3
+    good = 4
+    badness = 5
 
 
 # defining contract
@@ -84,9 +96,10 @@ class SmartContract:
         self.funds = funds      # total funds in the contract
         self.circuit_info = circuit_info
         self.proof_info = proof_info  # circuit verifier wants solved
+        self.state = 1
 
     def __str__(self):
-        return f"Staked funds: {self.funds}"
+        return( f"Staked funds: {self.funds}" + " Contract State: "+ str(contract_states(self.state).name))
 
     # burns funds in contract. burns all funds by default
     def burn(self, delta = 0):
@@ -101,24 +114,50 @@ class SmartContract:
     def z(self,p):
         return coin_flip(p)
 
-    def exicute(self, we_good, player_v, player_p):
-        if we_good == 0:
-            print("we good? " + str(we_good))
-            transfer(self, player_v.wallet, self.funds)
-        else:
-            transfer(contract, player_p.wallet, stake_p)
-            contract.burn()
-            print("burnt")
-
     def verify(self):
         my_input = self.circuit_info.data
         proof_output = self.proof_info.evaluate()
-        print("my input " + str(my_input)+"\nproof output " + str(proof_output))
+        # print("my input " + str(my_input)+"\nproof output " + str(proof_output))
         if my_input == proof_output:
             return True
         else:
-            print("boooo you lied")
             return False
+
+    def exicute_0(self):
+        # check correct funds, return
+        if self.funds != self.stake_p + self.stake_v:  # this is a tautology, should change
+            transfer(self, verifier, stake_v)
+            transfer(self, prover, stake_p)
+            self.state = 5
+        else:
+            self.state = 2
+
+    def exicute_1(self, p):
+        if self.state == 2:
+            z = coin_flip(p)
+
+            if z == 0:
+                self.state = 4
+            else:
+                print("check")
+                self.state = 3
+
+    def exicute_verify(self):
+        check = self.verify() # contract verifies proof
+        if check == True:
+            self.state = 4
+        else:
+            self.state = 5
+
+    def exicute_2(self):
+        if self.state == 4:
+            transfer(self, self.verifier.wallet, self.funds) # pay verifier
+        elif self.state == 5:
+            transfer(self, self.prover.wallet, self.funds)
+            self.burn()
+            print("burnt")
+        else:
+            print("panic")
 
 
 def main():
@@ -137,6 +176,10 @@ def main():
     proof = lambda y:int(math.sqrt(y))
     player_p = Prover(wallet_p, proof)
 
+    print(player_p.bit)
+    print(player_p.hash)
+
+
     # contract info
     stake_v = 40 # staking value alpha from notes
     stake_p = 50 # staking value lambda from notes
@@ -146,33 +189,28 @@ def main():
     # verifier stakes funds and sends info
     transfer(player_v.wallet, contract, contract.stake_v) # stake
 
-    player_p.circuit_info = player_v.circuit_info # send info packet
-    contract.circuit_info = player_v.circuit_info
-    print("I want my input, " + str(player_p.circuit_info.data) + ", squared.")
+    player_p.circuit_info = player_v.circuit_info # send info packet to prover
+    contract.circuit_info = player_v.circuit_info # send info packet to contract
+    print("P: I want my input, " + str(player_p.circuit_info.data) + ", squared.")
 
     # prover stakes funds, evaluates and 'creates proof'
     transfer(player_p.wallet, contract, stake_p) # stake funds
+
+    contract.exicute_0() # check correct staking
+
     player_p.proof_info.data = player_p.circuit_info.evaluate() # do computation
-    print("result of computation: "+ str(player_p.proof_info.data))
+    print("V: Result of computation: "+ str(player_p.proof_info.data))
 
-    player_v.proof_info = player_p.proof_info # transfer proof and result to verifier
+    player_v.proof_info = player_p.proof_info.data # result to verifier
 
-    z = contract.z(p) # contract creates randomness
+    contract.exicute_1(p)
 
-    we_good = 1 # initialize contract status
+    if contract.state == 3: # 3 means verify
+        contract.proof_info = player_p.proof_info # prover sends proof to contract
+        contract.exicute_verify()
 
-    if z == 0:
-        we_good = 0
-    else:
-        print("check")
-        contract.proof_info = player_v.proof_info # does v send this or does p?
-        check = contract.verify() # contract verifies proof
-        if check == True:
-            we_good = 0
-        else:
-            we_good = 1
+    contract.exicute_2()
 
-    contract.exicute(we_good, player_v, player_p)
     print(player_v.wallet)
     print(player_p.wallet)
     print(contract)
