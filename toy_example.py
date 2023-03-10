829414fa24b448057aaa52fd9184a291c5703180
Daji Landis <daji@ucla.edu>
2023-03-10 17:18:11 +0100

    finished hash mechanism.

--- toy_example.py
+++ toy_example.py
@@ -14,21 +14,24 @@ def transfer(sender, receiver, amount):
         receiver.deposit(amount)
 
 # coin flip function
-def coin_flip(p = 0.5):
-    random.seed(datetime.now().timestamp())
+def coin_flip(p = 0.5, seed = 0):
+    if seed == 0:
+        seed = datetime.now().timestamp()
+    random.seed(seed)
     result = random.random()
     if result <= p:
         return 1
     else:
         return 0
 
-def hash(bit):
-    if type(bit) is not bin:
-        bit = bin(bit)
+def hash(r):
+    if type(r) is not bin:
+        r = bin(r).encode('utf-8')
     m = hashlib.sha256()
-    m.update(b"Nobody inspects the spammish repetition")
+    m.update(r)
     m.hexdigest()
     return m
+
 
 # defining player's information for computation
 class InfoPacket:
@@ -62,20 +65,28 @@ class Verifier:
         self.wallet = wallet
         self.circuit_info = circuit_info
         self.proof_info = 0
-        self.bit = coin_flip(p=0.5)
-        self.hash = hash(self.bit)
+        self.r = random.randint(10000, 100000)
+        # self.hash = hash(self.r)
+
+    def player_commit_hash(self, contract):
+        my_hash = hash(self.r)
+        contract.commit_hash(id = "Vivian", hash = my_hash)
 
 class Prover:
     def __init__(self, wallet, proof):
         self.wallet = wallet
         self.circuit_info = InfoPacket()
         self.proof_info = InfoPacket(0, proof)
-        self.bit = coin_flip(p=0.5)
-        self.hash = hash(self.bit)
+        self.r = random.randint(10000, 100000)
+        # self.hash = hash(self.r)
 
     def do_computation(self):
         # do the computation and save to proof info packet
         self.proof_info.data = self.circuit_info.evaluate()
+
+    def player_commit_hash(self, contract):
+        my_hash = hash(self.r)
+        contract.commit_hash(id = "Petra", hash = my_hash)
 
 class contract_states(Enum):
     start = 1
@@ -96,7 +107,9 @@ class SmartContract:
         self.funds = funds      # total funds in the contract
         self.circuit_info = circuit_info
         self.proof_info = proof_info  # circuit verifier wants solved
-        self.state = 1
+        self.state = 1 # start
+        self.v_hash = 0
+        self.p_hash = 0
 
     def __str__(self):
         return( f"Staked funds: {self.funds}" + " Contract State: "+ str(contract_states(self.state).name))
@@ -123,36 +136,66 @@ class SmartContract:
         else:
             return False
 
+    def commit_hash(self, id, hash):
+        if id == "Vivian" and self.v_hash == 0:
+            self.v_hash = hash
+        elif id == "Petra" and self.p_hash == 0:
+            self.p_hash = hash
+        else:
+            print("commit failed")
+
+    def check_hash(self, r, old_hash):
+        old_hash = old_hash.hexdigest()
+        new_hash = hash(r).hexdigest()
+        if new_hash == old_hash:
+            return True
+        else:
+            print("old ")
+            print(old_hash.hexdigest())
+            print("new ")
+            print(new_hash.hexdigest())
+            return False
+
     def exicute_0(self):
         # check correct funds, return
         if self.funds != self.stake_p + self.stake_v:  # this is a tautology, should change
             transfer(self, verifier, stake_v)
             transfer(self, prover, stake_p)
-            self.state = 5
+            self.state = 5 # badness
         else:
-            self.state = 2
+            self.state = 2 # committed
 
-    def exicute_1(self, p):
+    def exicute_1(self, p, r_v, r_p):
+        check_1 = self.check_hash(r_v, self.v_hash)
+        check_2 = self.check_hash(r_p, self.p_hash)
+
+        if check_1 and check_2 == True:
+            self.state = 2
+        else:
+            self.state = 5
+
+        seed = r_v ^ r_p
+
         if self.state == 2:
-            z = coin_flip(p)
+            z = coin_flip(p, seed)
 
             if z == 0:
-                self.state = 4
+                self.state = 4  # good
             else:
                 print("check")
-                self.state = 3
+                self.state = 3 # verify
 
     def exicute_verify(self):
         check = self.verify() # contract verifies proof
         if check == True:
-            self.state = 4
+            self.state = 4 # good
         else:
-            self.state = 5
+            self.state = 5 # badness
 
     def exicute_2(self):
-        if self.state == 4:
+        if self.state == 4: # good
             transfer(self, self.verifier.wallet, self.funds) # pay verifier
-        elif self.state == 5:
+        elif self.state == 5: # badness
             transfer(self, self.prover.wallet, self.funds)
             self.burn()
             print("burnt")
@@ -176,8 +219,8 @@ def main():
     proof = lambda y:int(math.sqrt(y))
     player_p = Prover(wallet_p, proof)
 
-    print(player_p.bit)
-    print(player_p.hash)
+    # print(player_p.r)
+    # print(player_p.hash)
 
 
     # contract info
@@ -191,19 +234,24 @@ def main():
 
     player_p.circuit_info = player_v.circuit_info # send info packet to prover
     contract.circuit_info = player_v.circuit_info # send info packet to contract
-    print("P: I want my input, " + str(player_p.circuit_info.data) + ", squared.")
+    print("V: I want my input, " + str(player_v.circuit_info.data) + ", squared.")
 
     # prover stakes funds, evaluates and 'creates proof'
     transfer(player_p.wallet, contract, stake_p) # stake funds
 
     contract.exicute_0() # check correct staking
 
+    # contract.v_hash = player_v.hash
+    # contract.p_hash = player_p.hash
+    player_v.player_commit_hash(contract)
+    player_p.player_commit_hash(contract)
+
     player_p.proof_info.data = player_p.circuit_info.evaluate() # do computation
-    print("V: Result of computation: "+ str(player_p.proof_info.data))
+    print("P: Result of computation: "+ str(player_p.proof_info.data))
 
     player_v.proof_info = player_p.proof_info.data # result to verifier
 
-    contract.exicute_1(p)
+    contract.exicute_1(p, r_v = player_v.r, r_p = player_p.r)
 
     if contract.state == 3: # 3 means verify
         contract.proof_info = player_p.proof_info # prover sends proof to contract
