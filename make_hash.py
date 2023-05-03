df18edd15fc4c55edd879c6a620ec4395a17d1aa
Daji Landis <daji@ucla.edu>
2023-05-03 13:16:47 +0200

    little script for making the correct hash

---
+++ make_hash.py
@@ -0,0 +1,13 @@
+import sha3
+
+# Create a SHA3-256 hash object
+hasher = sha3.keccak_256()
+
+# Convert the input integer to bytes and hash it
+input_int = 385
+input_bytes = input_int.to_bytes(32, byteorder='big')
+hasher.update(input_bytes)
+hash_hex = hasher.hexdigest()
+
+# Print the hash value in hexadecimal format
+print(f"0x{hash_hex}")
