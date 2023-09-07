dfbab9049fbde2cb0195232ce40e54a54f111460
Daji Landis <daji@ucla.edu>
2023-09-07 16:16:32 +0200

    final version copied down from remix

--- make_hash.py
+++ make_hash.py
@@ -4,7 +4,7 @@ import sha3
 hasher = sha3.keccak_256()
 
 # Convert the input integer to bytes and hash it
-input_int = 385
+input_int = 801
 input_bytes = input_int.to_bytes(32, byteorder='big')
 hasher.update(input_bytes)
 hash_hex = hasher.hexdigest()
